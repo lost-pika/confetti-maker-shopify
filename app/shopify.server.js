c1a62c0b402b2f9c91bf69f256450ae66cc59ce4
Srishti Verma <srishtiverma358@gmail.com>
2025-12-26 20:38:46 +0530

    fixing ui visibility issue

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -5,13 +5,13 @@ import {
   shopifyApp,
 } from "@shopify/shopify-app-react-router/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
-import prisma from "./db.server";
+import prisma from "./db.server.js";
 
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
   apiVersion: ApiVersion.October25,
-  scopes: process.env.SCOPES?.split(","),
+  scopes: process.env.SCOPES ? process.env.SCOPES.split(",") : undefined,
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
@@ -22,6 +22,7 @@ const shopify = shopifyApp({
 });
 
 export default shopify;
+
 export const apiVersion = ApiVersion.October25;
 export const addDocumentResponseHeaders = shopify.addDocumentResponseHeaders;
 export const authenticate = shopify.authenticate;

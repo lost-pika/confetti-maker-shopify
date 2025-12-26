a3a72b45d87e4c6e7cf27bef4624a9c48715ecba
Srishti Verma <srishtiverma358@gmail.com>
2025-12-26 20:55:32 +0530

    fix: restructure app routes for react router

--- app/routes/app/_index.jsx
+++ app/routes/app/_index.jsx
@@ -1,6 +1,6 @@
 // app/routes/app._index.jsx
-import ConfettiApp from "../src/components/ConfettiApp";
-import { ShopProvider } from "../src/context/ShopContext";
+import ConfettiApp from "../../src/components/ConfettiApp";
+import { ShopProvider } from "../../src/context/ShopContext";
 
 export default function AppIndex() {
   return (
